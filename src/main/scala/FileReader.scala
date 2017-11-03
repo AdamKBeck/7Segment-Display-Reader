31c60eb7749fa62096f20d7af283e769a58b039d
Adam Beck <akb93@case.edu>
2017-11-02 21:33:25 -0400

    Added description to FileReader

--- src/main/scala/FileReader.scala
+++ src/main/scala/FileReader.scala
@@ -1,3 +1,8 @@
-class FileReader {
+/* This class reads an input text file containing the numbers. FileReader will try to split up each of the 3 lines of
+ * 27 characters into 9 number objects. This class does not check if each number is valid. It only checks the validity of
+ * if the input file is of the correct size and if each character is of the corrrect type (e.g. only using "|" and "-")
+ */
+
+case class FileReader() {
 
 }
