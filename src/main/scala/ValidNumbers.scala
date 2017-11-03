08cc2c7a1c74ce07aac65b0c513e1e16228c9d42
Adam Beck <akb93@case.edu>
2017-11-02 21:53:52 -0400

    Created ValidNumbers class and commented its outline

---
+++ src/main/scala/ValidNumbers.scala
@@ -0,0 +1,18 @@
+/* This class stores only the valid number 0-9. Each number is a list (because it needs to be ordered) of 7 booleans
+ * denoting whether or not the number has a certain segment.
+ *
+ *
+  * The possible segments are:
+  * _
+ * |_|
+ * |_|
+ *
+ * which has the segments (reading line by line):
+ *  1
+ * 234
+ * 567
+ */
+
+case class ValidNumbers() {
+
+}
