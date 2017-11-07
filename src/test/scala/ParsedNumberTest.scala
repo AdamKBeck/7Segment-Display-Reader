d381bd468181d50a915e5ce07a1874cb27a4e1f3
Adam Beck <akb93@case.edu>
2017-11-07 04:21:24 -0500

    ParsedNumberTest completed

---
+++ src/test/scala/ParsedNumberTest.scala
@@ -0,0 +1,33 @@
+package number_recognition
+
+import org.scalatest.FlatSpec
+
+class ParsedNumberTest extends FlatSpec {
+
+	// Good data: segments is not empty, min configuration of 1 segment
+	behavior of "segments"
+	it should "test a normal input, min confuration" in {
+		val list = List(1)
+		val number = ParsedNumber(list)
+
+		assert(number.segments == List(1))
+	}
+
+	// Good data: normal/max configuration: 7 segments
+	behavior of "segments"
+	it should "test normal/max config" in {
+		val list = List(9,8,7,6,5,4,3,2,1)
+		val number = ParsedNumber(list)
+
+		assert(number.segments == List(1,2,3,4,5,6,7,8,9))
+	}
+
+	// Bad data: Nil segments list
+	behavior of "segments"
+	it should "test with a Nil segment list" in {
+		val list = Nil
+		val number = ParsedNumber(list)
+
+		assert(number.segments == Nil)
+	}
+}
